fca8f6dbdd0a589ab8d20cf0afe5d9f303a71978
Dominic <dgiglio-tos1@gulls.salisbury.edu>
2018-04-04 16:44:16 -0400

    tiles and stuff
    has tiles, buttons, camera is formated, bits are formated, peices are added, movement has been started, ect.

---
+++ Assets/Moving.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour {
+	//yellow tiles are help tiles.
+	// Use this for initialization
+	void Start () {
+		//left and right movement is 6.4 per tile to move accross board, works like noraml x-y axis.
+		//up and down movement is 
+		transform.position = new Vector3 (38f, 28f, -9.6f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
