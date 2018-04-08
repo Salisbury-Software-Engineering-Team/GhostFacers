b9e1a653d7120ae6e20d6b9406f634b5d049b764
Matt Matuk <mjmatuk6088@outlook.com>
2018-04-07 21:46:09 -0400

    Added player stats script. Added AngelPiece prefab.
    Use the angel Piece prefab to test make add scripts and make changes to
    test the movement

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,13 @@
+﻿//***********************************************************
+// Created By:
+// Description: Use this for the charater stats of each charater
+//***********************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    //TODO: Finish adding stats
+
+}
