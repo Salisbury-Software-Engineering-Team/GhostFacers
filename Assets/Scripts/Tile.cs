780a9de10903c8999aa1ad181f0dd8842a20b6e1
Matt Matuk <mjmatuk6088@outlook.com>
2018-04-08 20:53:49 -0400

    Player Charater Prefab created and Clickable

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+
+    public void TestClicked()
+    {
+        Debug.Log("Clicked Tile");
+    }
+
+}
