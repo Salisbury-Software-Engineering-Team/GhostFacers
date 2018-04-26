8f87cbcc31c5a4b99cdbb87641f4562d8c4df315
Jimmy Sigg <jsigg1@gulls.salisbury.edu>
2018-04-26 15:56:57 -0400

    First Card Push
    Updated the code in card class and made Sam and Dean

--- Assets/Board/Scripts/Card.cs
+++ Assets/Board/Scripts/Card.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-[Serializable]
-public class Card
+//[Serializable]
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards")]
+public class Card : ScriptableObject
 {
 
     // needs changing
     public string Name;
-    public string Stats;
+    public int Health;
+    public int Attack;
 
-    public Card(string N, string S) { Name = N; Stats = S; }
+    public Sprite artwork;
+
+    public string Description; 
+    //for humans, says number of weapons and help character can hold
+    //for other cards, says their effect or is left blank
+    //non-humans can hold up to 2 cards by default
+
+    //used for summonable cards
+    public Card(string N, int H, int A, string D) { Name = N; Health = H; Attack = A; Description = D; }
+    //used for effect cards that can't be summoned
+    public Card(string N, string D) { Name = N; Description = D; }
+
 }
