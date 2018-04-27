4cb2ae0807d5a4ffcff6ffab36104646735cb756
Matt Matuk <coolskater6088@hotmail.com>
2018-04-27 15:09:35 -0400

    Added Effect Script. Added Somemore attributes to CharacterStat
    The Efffect script will be used for the createion of effect. This class will be extended by child classes.

--- Assets/Board/Scripts/CharacterStat.cs
+++ Assets/Board/Scripts/CharacterStat.cs
@@ -8,25 +8,19 @@ public class CharacterStat : ScriptableObject
     public String Name;
 
     //TODO: Finish adding stats
-    [SerializeField]
-    private int HealthLeft;
-    [SerializeField]
-    private int HealthTotal;
-    [SerializeField]
-    private int Movement;
+    [SerializeField] private int HealthLeft;
+    [SerializeField] private int HealthTotal;
+    [SerializeField] private int Movement;
 
-    public List<Card> Weapons;
-    [SerializeField]
-    private List<Card> Help;
+    [SerializeField] private readonly int MaxWeapons;
+    [SerializeField] private readonly int MaxHelp;
+
+    [SerializeField] private List<Card> Weapons;
+    [SerializeField] private List<Card> Help;
+    [SerializeField] private List<Effect> ActiveEffects;
 
     public SideType Side;
 
     public GameObject Model;
-
     public Color PieceColor;
-
-    //Needs some type of effect script.
-
-
-
 }
