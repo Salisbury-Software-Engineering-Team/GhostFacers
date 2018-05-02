d99afc98a64203dc872d602cdfa84bebd1dc6861
Matt Matuk <coolskater6088@hotmail.com>
2018-05-02 14:35:56 -0400

    Added UIStat script
    The UIStat script wil be used to display the currently selected characters piece info, ie attack and health points.

---
+++ Assets/UI/Script/UIStat.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStat : MonoBehaviour
+{
+    public GameObject AttackPanel;
+    public GameObject HealthPanel;
+
+    private CharacterPiece CurrentPiece;
+
+
+    private void Update()
+    {
+        if (DidCurrentPieceChange())
+            DisplayCharacterStat();
+    }
+
+    private void Start()
+    {
+        if (GameManager.instance.CurrentPiece != null)
+        {
+            CurrentPiece = GameManager.instance.CurrentPiece;
+        }
+    }
+
+    private void DisplayCharacterStat()
+    {
+        if (CurrentPiece != null)
+        {
+            // Turn on the panels
+            AttackPanel.SetActive(true);
+            HealthPanel.SetActive(true);
+        }
+        else
+        {
+            // Hide Panels bc no character is selected
+            AttackPanel.SetActive(false);
+            HealthPanel.SetActive(false);
+        }
+    }
+
+    private bool DidCurrentPieceChange()
+    {
+        if (GameManager.instance.CurrentPiece != CurrentPiece) // current piece seleced changed
+        {
+            CurrentPiece = GameManager.instance.CurrentPiece;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
