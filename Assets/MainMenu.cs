e72697408d0f99db8e7a8a95198affa8ed7f11df
Matt Matuk <mjmatuk6088@outlook.com>
2018-05-17 20:38:16 -0400

    Semi Working Game

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
 	public void PlayGame(){
-		SceneManager.LoadScene ("StyleOne");
+		SceneManager.LoadScene ("Board/Scenes/Boards/Main_Board");
 
 
 	}
