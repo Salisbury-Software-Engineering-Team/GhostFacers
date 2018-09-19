bfa404d40a8f3d66f357ffb12a3c526111f6ae1d
Matt Matuk <coolskater6088@hotmail.com>
2018-09-19 15:46:21 -0400

    Add Effect Script
    Started to create the effect base script that all of our effects will draw from.

--- Assets/Cards/Scripts/Effects/Effect.cs
+++ Assets/Cards/Scripts/Effects/Effect.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Effect : MonoBehaviour
+public abstract class Effect : ScriptableObject
 {
     // TODO: Add pahse variable that will be used to determine when the effect is appiled
     // This variable will mainly be used to effect that last mutiple turns.
 
     // how many more turn the effect should continued to be applied to the piece.
     // when could use -1 to mean infinite.
-    public int TurnsLeft; 
+    public int TurnsLeft;
+    public string Name = "New Effect"; // Name of the effect
+    public Phase ActivatePhase; // phase that the card can be played.
 
-    // TODO added function for Effects
+    public abstract void Initialize(GameObject obj);
+    public abstract void TriggerEffect();
 
 }
