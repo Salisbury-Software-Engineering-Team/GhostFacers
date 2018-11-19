24995e452546a2c4f467cc24103fa190f926b057
Matt Matuk <coolskater6088@hotmail.com>
2018-11-19 12:47:38 -0500

    Adding UI to chose effect when multiple are present.

---
+++ Assets/Cards/Scripts/Effects/ChoiceEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ChoiceEffect : Effect
+{
+    protected List<PreFab> ChoicesPreFabs;
+    public override void ToggleActivation()
+    {
+        
+    }
+
+}
